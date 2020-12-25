84f1c8f5893762c36c031d32f06ba32df6bd85b8
Nikita <miracleboy3228@mail.ru>
2020-12-25 21:12:31 +0300

    Правит модальное окно и дописывает js

--- source/js/main.js
+++ source/js/main.js
@@ -1,20 +1,54 @@
 let navMain = document.querySelector('.main-nav');
-    let navToggle = document.querySelector('.main-nav__toggle');
+let navToggle = document.querySelector('.main-nav__toggle');
 
-    navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--nojs');
 
-    navToggle.addEventListener('click', function () {
-      navMain.classList.toggle('main-nav--closed');
-    });
+navToggle.addEventListener('click', function () {
+  navMain.classList.toggle('main-nav--closed');
+});
 
-let weekButton = document.querySelector('.week-product__button');
-let productButton = document.querySelector('.products__button');
 let modal = document.querySelector('.modal');
+let modalButton = document.querySelectorAll('.js-modal-button');
 
-weekButton.addEventListener('click', function() {
-  modal.classList.add('modal--show');
+for (let i = 0; i < modalButton.length; i++) {
+  modalButton[i].onclick = function () {
+    modal.classList.add('modal--show');
+  };
+}
+
+modal.addEventListener('click', function (e) {
+  if (e.target === modal) {
+    modal.classList.remove('modal--show');
+  }
 });
 
-productButton.addEventListener('click', function() {
-  modal.classList.add('modal--show');
-});
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlide() {
+  showSlides(slideIndex += 1);
+}
+
+function minusSlide() {
+  showSlides(slideIndex -= 1);
+}
+
+function showSlides(n) {
+
+  let slides = document.querySelector(".reviews__item");
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
